ae43678d823d59a63531ba0f1725f5eb4f1c8b3b
Ronaldoengenhariadacomputacao <126127042+Ronaldoengenhariadacomputacao@users.noreply.github.com>
2025-04-09 16:12:29 -0300

    Update test_interface.py

--- tests/test_interface.py
+++ tests/test_interface.py
@@ -1,8 +1,15 @@
 import unittest
+from qiskit import QuantumCircuit
+from autoq.interface import QiskitInterface
 
 class TestQiskitInterface(unittest.TestCase):
     def test_get_gate_count(self):
-        pass
+        circuit = QuantumCircuit(14)
+        for i in range(400):
+            circuit.h(i % 14)
+        interface = QiskitInterface(circuit)
+        gate_count = interface.get_gate_count()
+        self.assertEqual(gate_count, 400)
 
 if __name__ == "__main__":
     unittest.main()
