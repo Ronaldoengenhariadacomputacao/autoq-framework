fd46d1633da72a5a923f693b562e78789baa82e5
Ronaldoengenhariadacomputacao <126127042+Ronaldoengenhariadacomputacao@users.noreply.github.com>
2025-04-09 16:21:37 -0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,12 +1,10 @@
-from setuptools import setup, find_packages
-
 setup(
     name="autoq",
     version="1.0.0",
     packages=find_packages(),
     install_requires=["qiskit==0.43.0"],
     author="Ronaldo Rodrigues",
-    author_email="Ronaldoengenhariadacomputacao@hotmail.com",
+    author_email="Ronaldoengenhariadacomputacao@hotmail.com"
     description="AutoQ Framework: Automated quantum circuit optimization",
     long_description="The AutoQ Framework is a tool for automated quantum circuit optimization, designed to increase processing speed (gates per µs). It integrates transpilation, parallelism, and QEC into a unified pipeline.",
     long_description_content_type="text/plain",
